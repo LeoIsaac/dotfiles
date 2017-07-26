84d21044bd3de295e8b0eac340d3e33f1101ef4f
i544c <leo@i544c.me>
2017-07-26 22:59:16 +0900

    Add view error

--- conf.d/prompt.sh
+++ conf.d/prompt.sh
@@ -5,13 +5,13 @@ autoload -Uz is-at-least
 setopt correct
 
 PROMPT="%{$fg_bold[blue]%}%~%{$reset_color%}
-%{$fg_bold[green]%}>%{$fg_bold[yellow]%}>%{$fg_bold[red]%}>%{$reset_color%} "
+%{%(?.$fg_bold[green].$fg_bold[red])%}>%{%(?.$fg_bold[yellow].$fg_bold[red])%}>%{$fg_bold[red]%}>%{$reset_color%} "
 
 if is-at-least 4.3.10; then
     zstyle ':vcs_info:git:*' check-for-changes true
     zstyle ':vcs_info:git:*' stagedstr "+"
     zstyle ':vcs_info:git:*' unstagedstr "-"
-    zstyle ':vcs_info:*' formats "[%b] %c%u"
+    zstyle ':vcs_info:*' formats "%c%u[%b]"
     zstyle ':vcs_info:*' actionformats '[%b|%a]'
 fi
 
