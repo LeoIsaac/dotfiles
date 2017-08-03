aff88a2446828ce8e3f56847c55e818f2668fd47
i544c <leo@i544c.me>
2017-08-03 22:12:51 +0900

    Add install pyenv

--- init.d/install.sh
+++ init.d/install.sh
@@ -1,1 +1,17 @@
-git clone https://github.com/zplug/zplug $HOME/.zplug
+if [ -f $HOME/.zplug ]; then
+    git clone https://github.com/zplug/zplug $HOME/.zplug
+    source $HOME/.zshrc
+fi
+
+
+if [ -f $HOME/.pyenv ]; then
+    git clone https://github.com/pyenv/pyenv ~/.pyenv
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
+
+    cat <<'EOF' >> $HOME/.zshrc
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    EOF
+fi
