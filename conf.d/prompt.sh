0e2c86a0582f412bbd0f0f3b9bc931f79b654e1e
i544c <leo@i544c.me>
2018-02-18 17:13:23 +0900

    Change position of git status

--- conf.d/prompt.sh
+++ conf.d/prompt.sh
@@ -4,9 +4,6 @@ autoload -Uz is-at-least
 
 setopt correct
 
-PROMPT="%{$fg_bold[blue]%}%~%{$reset_color%}
-%{%(?.$fg_bold[green].$fg_bold[red])%}>%{%(?.$fg_bold[yellow].$fg_bold[red])%}>%{$fg_bold[red]%}>%{$reset_color%} "
-
 if is-at-least 4.3.10; then
     zstyle ':vcs_info:git:*' check-for-changes true
     zstyle ':vcs_info:git:*' stagedstr "+"
@@ -25,6 +22,9 @@ precmd() {
     vcs_info
     [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
-RPROMPT="%1(v|%1v%f|)"
+#RPROMPT="%1(v|%1v%f|)"
+
+PROMPT="%1(v|%1v%f |)%{$fg_bold[blue]%}%~%{$reset_color%}
+%{%(?.$fg_bold[green].$fg_bold[red])%}>%{%(?.$fg_bold[yellow].$fg_bold[red])%}>%{$fg_bold[red]%}>%{$reset_color%} "
 
 alias clear='clear; is_clear=1'
